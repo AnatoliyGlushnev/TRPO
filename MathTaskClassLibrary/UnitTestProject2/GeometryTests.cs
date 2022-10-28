c58b5790ac1ff7b3ea5f6a603f779dcee34ce750
-Twist <91672198+AnatoliyGlushnev@users.noreply.github.com>
2022-10-28 20:28:49 +0300

    Add files via upload

---
+++ MathTaskClassLibrary/UnitTestProject2/GeometryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibrary
+{
+    [TestClass]
+    public class GeomtetryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            Geometry g = new Geometry();
+            int actual = g.Rectaanglearea(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
